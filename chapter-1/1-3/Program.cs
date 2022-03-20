aee96c8036ae0150dd9d1922d293bcaa50075599
Constantine Colotiline <colotiline@outlook.com>
2022-03-20 16:50:23 +0300

    Added 1.3.

---
+++ chapter-1/1-3/Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(RemoveDuplicates("Hello, World!"));
+Console.WriteLine(RemoveDuplicates("aaa"));
+Console.WriteLine(RemoveDuplicates("abababa"));
+Console.WriteLine(RemoveDuplicates("No duplicates!"));
+
+static string RemoveDuplicates(string stringToRemoveDuplicates)
+{
+    if (stringToRemoveDuplicates.Length < 2)
+    {
+        return stringToRemoveDuplicates;
+    }
+
+    var stringWithoutDuplicates = string.Empty;
+
+    foreach (var character in stringToRemoveDuplicates)
+    {
+        if (stringWithoutDuplicates.Contains(character))
+        {
+            continue;
+        }
+
+        stringWithoutDuplicates += character;
+    }
+
+    return stringWithoutDuplicates;
+}
