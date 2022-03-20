45714b88ef3291447da29b1a0c35084cce3ee957
Constantine Colotiline <colotiline@outlook.com>
2022-03-20 16:21:34 +0300

    Added nullable comment.

--- chapter-1/1-2/Program.cs
+++ chapter-1/1-2/Program.cs
@@ -1,6 +1,9 @@
 ﻿Console.WriteLine(Reverse("!iH"));
 Console.WriteLine(Reverse("radar"));
+Console.WriteLine(Reverse(string.Empty));
 
+// Personally I wouldn't care about null string,
+// because of <Nullable>enable</Nullable>.
 static string Reverse(string stringToReverse)
 {
     var stringChars = stringToReverse.ToCharArray();
