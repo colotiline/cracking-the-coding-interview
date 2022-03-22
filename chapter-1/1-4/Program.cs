17dc041bd5cc65f62715a4afa667143a67face5c
Constantine Colotiline <colotiline@outlook.com>
2022-03-22 21:49:17 +0300

    Added 1.4.

---
+++ chapter-1/1-4/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine(IsAnagrams("banana", "apple"));
+Console.WriteLine(IsAnagrams("night", "thing"));
+
+static bool IsAnagrams(string oneString, string anotherString)
+{
+    var oneStringArray = oneString.ToArray();
+    var anotherStringArray = anotherString.ToArray();
+
+    Array.Sort(oneStringArray);
+    Array.Sort(anotherStringArray);
+
+    for (var i = 0; i < oneStringArray.Length; i++)
+    {
+        if (oneStringArray[i] != anotherStringArray[i])
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
