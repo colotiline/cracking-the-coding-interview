7a8fe13936080196c0be8f31197ec369c69b0443
Constantine Colotiline <colotiline@outlook.com>
2022-03-24 20:57:02 +0300

    Added 2-2.

---
+++ chapter-2/2-2/Program.cs
@@ -0,0 +1,30 @@
+﻿var linkedList = new LinkedList<int>();
+
+linkedList.AddLast(1);
+linkedList.AddLast(2);
+linkedList.AddLast(3);
+linkedList.AddLast(4);
+linkedList.AddLast(5);
+
+Console.WriteLine(FindNthToLast(linkedList, 1).Value);
+Console.WriteLine(FindNthToLast(linkedList, 4).Value);
+Console.WriteLine(FindNthToLast(linkedList, 0).Value);
+
+static LinkedListNode<int> FindNthToLast
+(
+    LinkedList<int> linkedList,
+    int n
+)
+{
+    var position = linkedList.Count - n;
+    var nthNode = linkedList.First;
+
+    while (position > 1)
+    {
+        nthNode = nthNode!.Next;
+
+        position--;
+    }
+
+    return nthNode!;
+}
