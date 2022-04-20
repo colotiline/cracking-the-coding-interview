a77dd657ba9c9f830f49042d8666339269158694
Constantine Colotiline <colotiline@outlook.com>
2022-04-20 18:09:14 +0300

    Added 3-4.

---
+++ chapter-3/3-4/Program.cs
@@ -0,0 +1,37 @@
+﻿var rode1 = new Stack<int>();
+var rode2 = new Stack<int>();
+var rode3 = new Stack<int>();
+
+var n = 5;
+
+for (var i = n; i > 0 ; i--) 
+{
+    rode1.Push(i);
+}
+
+Move(n, rode1, rode3, rode2);
+
+foreach (var item in rode3)
+{
+    Console.WriteLine(item);
+}
+
+static void Move
+(
+    int diskNumber, 
+    Stack<int> from,
+    Stack<int> to,
+    Stack<int> buffer
+)
+{
+    if (diskNumber == 0) 
+    {
+        return;
+    }
+
+    Move(diskNumber - 1, from, buffer, to);
+
+    to.Push(from.Pop());
+
+    Move(diskNumber - 1, buffer, to, from);
+}
